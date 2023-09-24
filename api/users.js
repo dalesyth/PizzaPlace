ba1cd079c7141a07b8bfaec5f014ed0ec2c29d10
Dale Syth <dalesyth@gmail.com>
2023-09-24 16:15:26 -0500

    added delete user api route

--- api/users.js
+++ api/users.js
@@ -8,6 +8,7 @@ const {
   createUser,
   getUser,
   getUserByUserId,
+  deleteUser,
 } = require("../db/users");
 const { requireUser, requireAdmin } = require("./utils");
 
@@ -161,6 +162,24 @@ usersRouter.get("/useremail/:email", async (req, res, next) => {
     next({ name, message });
   }
 });
+
+// DELETE /api/user/:userId
+
+usersRouter.delete("/:userId", requireAdmin, async(req, res, next) => {
+    const { userId } = req.params;
+    try {
+        const deletedUser = await deleteUser(userId);
+
+        if (deletedUser) {
+            res.status(200).send("User has been deleted");
+        } else {
+            res.status(404).send("User not found or deletion failed");
+        }
+    } catch ({ name, message }) {
+        console.error({ name, message });
+        next({ name, message });
+    }
+})
 
 module.exports = {
   usersRouter,
