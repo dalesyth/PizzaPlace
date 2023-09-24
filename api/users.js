807fad169a9040949705b031e711095011077dde
Dale Syth <dalesyth@gmail.com>
2023-09-24 14:10:02 -0500

    create user registration api route

--- api/users.js
+++ api/users.js
@@ -1,11 +1,60 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { REACT_APP_JWT_SCRET } = process.env;
+const { REACT_APP_JWT_SCRET, JWT_EXPIRATION_TIME } = process.env;
 const usersRouter = express.Router();
+const { getUserByEmail, createUser } = require("../db/users");
 
+usersRouter.post("/register", async (req, res, next) => {
+  const { first_name, last_name, email, password, phone } = req.body;
+  try {
+    if (password.length < 8) {
+      next({
+        message: "Password must be at least 8 characters",
+        name: "PasswordTooShortError",
+        error: "Password Too Short",
+      });
+    }
 
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      next({
+        message: `User already exists with email: ${email}`,
+      });
+    }
+
+    const newUser = await createUser({
+      first_name,
+      last_name,
+      password,
+      email,
+      phone,
+    });
+
+    const expirationTime =
+      Math.floor(Date.now() / 1000) + parseInt(JWT_EXPIRATION_TIME);
+
+    const token = jwt.sign(
+      {
+        id: newUser.id,
+        email: newUser.email,
+        exp: expirationTime,
+      },
+      REACT_APP_JWT_SCRET
+    );
+
+    res.status(201).send({
+      message: "Thank you for registering",
+      token,
+      email,
+    });
+  } catch ({ name, message }) {
+    console.error({ name, message });
+    next({ name, message });
+  }
+});
 
 module.exports = {
-    usersRouter
-}
+  usersRouter,
+};
