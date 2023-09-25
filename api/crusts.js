50b4b3219ee680f88da664060433090a53063217
Dale Syth <dalesyth@gmail.com>
2023-09-25 14:14:43 -0500

    added api routes to get crust info by id and title

--- api/crusts.js
+++ api/crusts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const crustsRouter = express.Router();
 const { requireAdmin } = require("./utils");
-const { getAllCrusts } = require("../db/crusts");
+const { getAllCrusts, getCrustById, getCrustByTitle } = require("../db/crusts");
 
 // GET /api/crusts
 
@@ -13,6 +13,40 @@ crustsRouter.get("/", async (req, res, next) => {
       res.status(404).send("Error getting crust info");
     } else {
       res.status(200).send(crusts);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// GET /api/crusts/:crustId
+
+crustsRouter.get("/:crustId", async (req, res, next) => {
+  const { crustId } = req.params;
+  try {
+    const crust = await getCrustById(crustId);
+
+    if (!crust || crust.length === 0) {
+      res.status(404).send("Crust not found");
+    } else {
+      res.status(200).send(crust);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// GET /api/crusts/title/:title
+
+crustsRouter.get("/title/:title", async (req, res, next) => {
+  const { title } = req.params;
+  try {
+    const crust = await getCrustByTitle(title);
+
+    if (!crust || crust.length === 0) {
+      res.status(404).send("Crust not found");
+    } else {
+      res.status(200).send(crust);
     }
   } catch ({ name, message }) {
     next({ name, message });
