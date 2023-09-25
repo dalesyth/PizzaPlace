42e010d5a98bc8915400f1d33c2835b37c5829d1
Dale Syth <dalesyth@gmail.com>
2023-09-24 21:40:27 -0500

    added api route to get topping by id

--- api/toppings.js
+++ api/toppings.js
@@ -1,30 +1,40 @@
 const express = require("express");
-const { getAllToppings } = require("../db/toppings");
+const { getAllToppings, getToppingById } = require("../db/toppings");
 const toppingsRouter = express.Router();
 
 // GET /api/toppings
 
 toppingsRouter.get("/", async (req, res, next) => {
-    try {
-        const toppings = await getAllToppings();
-        if (!toppings || toppings.length === 0) {
-            res.status(404).send('Toppings not found')
-        } else {
-            res.status(200).send(toppings)
-        }
-    } catch ({ name, message }) {
-        next({ name, message })
+  try {
+    const toppings = await getAllToppings();
+    if (!toppings || toppings.length === 0) {
+      res.status(404).send("Toppings not found");
+    } else {
+      res.status(200).send(toppings);
     }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
+// GET /api/toppings/:toppingId
 
+toppingsRouter.get("/:toppingId", async (req, res, next) => {
+  const { toppingId } = req.params;
 
+  try {
+    const topping = await getToppingById(toppingId);
 
-
-
-
-
+    if (!topping || topping.length === 0) {
+      res.status(404).send("Topping not found");
+    } else {
+      res.status(200).send(topping);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 module.exports = {
-    toppingsRouter,
+  toppingsRouter,
 };
