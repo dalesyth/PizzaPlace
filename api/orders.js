322b6d9ee242e864f5f1aca9a4adf0b13de18a73
Dale Syth <dalesyth@gmail.com>
2023-09-25 21:29:00 -0500

    added api route to get all open orders

--- api/orders.js
+++ api/orders.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const { getAllOpenOrders } = require('../db/orders')
 const ordersRouter = express.Router()
+
+// GET /api/orders
+
+ordersRouter.get("/", async (req, res, next) => {
+    try {
+        const orders = await getAllOpenOrders()
+
+        if (!orders || orders.length === 0) {
+            res.status(404).send("Unable to retrieve open orders")
+        } else {
+            res.status(200).send(orders)
+        }
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+})
+
+// GET /api/orders/:orderId
+
+orders
 
 
 
