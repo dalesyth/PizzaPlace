0ff206277883dd1ff7dbc34249c0b1f68413c53e
Dale Syth <dalesyth@gmail.com>
2023-09-29 21:42:50 -0500

    added api routes to retrieve ordered pizzas by pizza id and user id

--- api/orderedpizzas.js
+++ api/orderedpizzas.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const { getOrderedPizzaByPizzaId, getOrderedPizzasByUser } = require("../db/orderedpizzas");
 const orderedPizzaRouter = express.Router();
 
 // GET /api/ordered-pizza
 
-orderedPizzaRouter.get("/", async (req, res, next) => {
+orderedPizzaRouter.get("/:pizzaId", async (req, res, next) => {
+    const { pizzaId } = req.params
     try {
-        const orderedPizza = 
+        const orderedPizza = await getOrderedPizzaByPizzaId(pizzaId)
+
+        if (!orderedPizza || orderedPizza.length === 0) {
+            res.status(404).send("Unable to retrieve this ordered pizza")
+        } else {
+            res.status(200).send(orderedPizza)
+        }
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+})
+
+orderedPizzaRouter.get("/user/:userId", async (req, res, next) => {
+    const { userId } = req.params
+    try {
+        const orderedPizza = await getOrderedPizzasByUser(userId)
+
+        if (!orderedPizza || orderedPizza.length === 0) {
+            res.status(404).send("Unable to retrieve ordered pizza")
+        } else {
+            res.status(200).send(orderedPizza)
+        }
     } catch ({ name, message }) {
         next({ name, message })
     }
