a52462b4e6c5aee9d1e477b81d2025cca68ff9e4
Dale Syth <dalesyth@gmail.com>
2023-10-16 22:04:36 -0500

    added db method to update specialty pizza

--- db/specialtypizzas.js
+++ db/specialtypizzas.js
@@ -24,7 +24,35 @@ async function createSpecialtyPizza({ ...fields }) {
     } = await client.query(newOrderSQL, dataArray);
     return specialtyPizza;
   } catch (error) {
-    console.error("Error creating ordered pizza: ", error);
+    console.error("Error creating specialty pizza: ", error);
+    throw error;
+  }
+}
+
+async function updateSpecialtyPizza(id, ...fields) {
+  let dataArray = Object.values(fields[0]);
+  const setString = Object.keys(fields[0])
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(",");
+  const sql = `
+        UPDATE specialty_pizzas
+        SET ${setString}
+        WHERE pizza_id=$${dataArray.length + 1}
+        RETURNING *;
+    `;
+  dataArray.push(id);
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [specialtyPizza],
+    } = await client.query(sql, dataArray);
+    return specialtyPizza;
+  } catch (error) {
+    console.error("Error updating specialty pizza: ", error);
     throw error;
   }
 }
