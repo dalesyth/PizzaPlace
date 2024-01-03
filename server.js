5af3868181040936518a3bb5636fafc8adf96bec
Dale Syth <dalesyth@gmail.com>
2024-01-02 21:21:48 -0600

    updated port settings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.PORT || 3000;
 
 const app = express();
 
